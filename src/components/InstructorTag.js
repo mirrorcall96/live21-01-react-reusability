4ca6cc0b9d7043ef978421faaa0bafb5778ae122
mirrorcall96 <mirrorcall96@gmail.com>
2021-06-13 12:50:05 +0300

    solved

--- src/components/InstructorTag.js
+++ src/components/InstructorTag.js
@@ -1,7 +1,13 @@
-const InstructorTag = () => {
-  // Create the InstructorTag component here
+import {TagWrapper,Emoji,Name,GoToGithub} from './styles'
 
-  return <></>;
+const InstructorTag = (props) => {
+  // Create the InstructorTag component here
+  let instructor = props;
+  return <><TagWrapper onClick={() => window.open(`https://github.com/${instructor.github}`)}>
+    <Emoji>{instructor.emoji}</Emoji>
+    <Name>{instructor.name}</Name>
+    <GoToGithub>Go to GitHub</GoToGithub>
+  </TagWrapper></>
 };
 
 export default InstructorTag;
